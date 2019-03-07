3dff6870f014137e7d25f9caa7566040d3fcc7f9
Gregoire BURNET <44721323+Webgburnet@users.noreply.github.com>
2019-03-07 22:03:02 +0100

    Add files via upload

---
+++ Gaz/Gaz.ino
@@ -0,0 +1,31 @@
+#include <Wire.h>
+#include <MutichannelGasSensor.h>
+
+void setup() 
+{
+  Serial.begin(115200);
+  Serial.println("Début setup");
+
+  gas.begin(0x04);
+  gas.powerOn();
+  Serial.print("Version du Firmware : ");
+  Serial.println(gas.getVersion());
+  
+  delay(1000);
+  
+  Serial.println("Fin setup");
+}
+
+void loop() {
+  float co;
+  float no2;
+  co = gas.measure_CO();   
+  no2 = gas.measure_NO2();
+
+  Serial.print("Concentration de CO : ");
+  Serial.print(co);
+  Serial.println(" ppm");
+  Serial.print("Concentration de NO2 : ");
+  Serial.print(no2);
+}
+
